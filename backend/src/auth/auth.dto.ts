0fb90d447779d3d9729b752003fa9ce6261592c0
kersor <vasilev.rustik.03@mail.ru>
2024-08-01 02:49:53 +0300

    Добавил: Валидацию при регистрации

--- backend/src/auth/auth.dto.ts
+++ backend/src/auth/auth.dto.ts
@@ -1,5 +1,19 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
 export class AuthCreateDto {
+    @IsString({
+        message: "Поле имя должно быть заполнено"
+    })
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsString({
+        message: "Поле пароль должно быть заполено"
+    })
+    @MinLength(8, {
+        message: 'Минимальное количество символов должно быть 8'
+    })
     password: string;
 }
